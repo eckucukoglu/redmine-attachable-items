eb36146f5d9e64ed2720290fd9a68fc61f254747
Emre Can Kucukoglu <eckucukoglu@gmail.com>
2016-06-23 17:19:29 +0300

    fix #3

--- lib/items_hook_listener.rb
+++ lib/items_hook_listener.rb
@@ -3,22 +3,24 @@ class ItemsHookListener < Redmine::Hook::ViewListener
   render_on :view_issues_show_description_bottom, :partial => "items/issues_show_item"
 
   def controller_issues_new_after_save(context={})
+    @project = Project.find(context[:issue][:project_id])
     @item_id = context[:params][:items][:item_id]
-    @issue_id = context[:issue][:id]
 
-    @itemsissue = ItemsIssues.find_by_issues_id(@issue_id)
-    if @itemsissue == nil
-      @itemsissue = ItemsIssues.new(:items_id => @item_id, :issues_id => @issue_id)
-      @status = @itemsissue.save
-    else
-      @status = @itemsissue.update_attributes(:items_id => @item_id)
+    if @project.module_enabled?("items") == true && @item_id != ""
+      @issue_id = context[:issue][:id]
+      @itemsissue = ItemsIssues.find_by_issues_id(@issue_id)
+      if @itemsissue == nil
+        @itemsissue = ItemsIssues.new(:items_id => @item_id, :issues_id => @issue_id)
+        @status = @itemsissue.save
+      else
+        @status = @itemsissue.update_attributes(:items_id => @item_id)
+      end
+
+      unless @status
+        Rails.logger.info(@itemsissue.errors.inspect)
+      end
     end
-
-    unless @status
-      Rails.logger.info(@itemsissue.errors.inspect)
-    end
-
-    return ''
+      return ''
   end
 
   alias_method :controller_issues_edit_after_save, :controller_issues_new_after_save
